9c9f4056f41f1527775d9a051850f3d2e8b89e56
SzymonMajk <Szymek_diks@interia.pl>
2017-05-28 21:57:28 +0200

    Exercises 2 and 3 from 2 chapter

---
+++ Exercises/Chapter2.hs
@@ -0,0 +1,12 @@
+--function, that returns the element before the last
+lastButOne :: [a] -> a
+lastButOne [] = error "List is empty"
+lastButOne [_] = error "List have only one element"
+lastButOne (x:y:[]) = x
+lastButOne (x:y:z) = lastButOne (y:z)
+
+--lastButOne []
+--lastButOne [2]
+--lastButOne [2,4]
+--lastButOne [3,5,6]
+--lastButOne ['A'..'Z']
