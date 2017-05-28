ddd1b1050c85f592e61c9b4e768af728418fe34c
SzymonMajk <Szymek_diks@interia.pl>
2017-05-29 00:24:36 +0200

    Second part of exercises from chapter 2

--- Exercises/Chapter3.hs
+++ Exercises/Chapter3.hs
@@ -57,8 +57,31 @@ treeHeigh (Node _ left right) = 1 + max (treeHeigh left) (treeHeigh right)
 --treeHeigh Empty
 --treeHeigh (Node 5 (Node 4 Empty (Node 2 Empty (Node 4 Empty Empty))) Empty)
 -------------------------------------------------------------------------------
---function 
+--function that return if third argument is to the left, right or create a straight line with first and sedond argument, arguments are two dimensional points
 
+data Direction = LeftTurn | RightTurn | Straight deriving (Show)
 
+findDirection :: (Num a, Ord a) => (a, a) -> (a, a) -> (a, a) -> Direction
+findDirection (x1,y1) (x2,y2) (x3,y3)
+  | c > 0 = LeftTurn
+  | c < 0 = RightTurn
+  | otherwise = Straight 
+  where c = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) 
 
+--findDirection (0,3) (0,3) (0,2)
+--findDirection (0,3) (0,3) (0,2)
+--findDirection (0,3) (0,3) (0,2)
+-------------------------------------------------------------------------------
+--function that compute list of Directions from list of points, direction for every three next point
 
+showDirections :: (Num a, Ord a) => [(a, a)] -> [Direction]
+showDirections [] = []
+showDirections (x:[]) = []
+showDirections (x:y:[]) = []
+showDirections (x:y:z:r) = ((findDirection x y z) : (showDirections (y:z:r)))
+
+--showDirections [(0,3),(0,2),(0,1),(4,2),(2,-2)]
+-------------------------------------------------------------------------------
+--funtion that Graham algorithm for searching convex hull
+
+--findConvexHull :: ...
