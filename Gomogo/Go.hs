8e52163f85febbe09566f0bebe068880029084fe
SzymonMajk <Szymek_diks@interia.pl>
2017-05-31 10:18:33 +0200

    Szkic funkcji oceniajacej

--- Gomogo/Go.hs
+++ Gomogo/Go.hs
@@ -1,22 +1,27 @@
 import qualified Data.Matrix as M
 import qualified Data.List as L
 
-data Pawn = Color Char | Blank | OutOfBoard deriving (Eq)
+data Pawn = White | Black | Blank | OutOfBoard deriving (Eq)
 
 data GameBoard = GameBoard {board :: (M.Matrix Pawn)}
 
+-------------------------------------------------------------------------------
+
 instance Show Pawn where
-  show (Color c) = show c
+  show (White) = show 'o'
+  show (Black) = show 'x'
   show (Blank) = show " "
 
 instance Show GameBoard where
   show (GameBoard b) = unlines ( map ((take 1 z)++) (lines (show b))) ++ "    " ++ L.intercalate "   " [[c] | c <- z]
 
+-------------------------------------------------------------------------------
+
 initializeBoard = GameBoard $ M.matrix size size (\(i,j) -> Blank)
 
-putPawn c k b = GameBoard $ M.setElem (Color c) k (board b)
+putPawn col coords b = GameBoard $ M.setElem col coords (board b)
 
--- np. putPawn 'o' (3,5) $ putPawn 'x' (3,6) $ putPawn 'o' (2,5) initializeBoard
+-- np. putPawn White (3,5) $ putPawn Black (3,6) $ putPawn White (2,5) initializeBoard
 
 z = take size ['A'..]
 size = 15
@@ -24,9 +29,21 @@ size = 15
 --wolne pola
 freeList (GameBoard b) = [(x,y)| x <-[1..size], y <- [1..size], (M.getElem x y b) == Blank]
 
---ocena planszy
-boardRating board player = undefined
+-------------------------------------------------------------------------------
 
+boardRate :: GameBoard -> Pawn -> Int
+boardRate (GameBoard b) player = sum $ findNearPositions player (findPlayerPositions b player)
+
+findPlayerPositions :: Eq a => M.Matrix a -> a -> [(Int, Int)]
+findPlayerPositions b player = [(x,y)| x <-[1..size], y <- [1..size], (M.getElem x y b) == player]
+
+findNearPositions :: Num b => t -> [(b, b)] -> [b]
+findNearPositions player coords = map (ratePoint player) coords
+
+ratePoint :: Num a => t -> (a, a) -> a
+ratePoint player coords = (fst coords) + (snd coords)
+
+-------------------------------------------------------------------------------
 
 newtype GameTree = Tree GameBoard deriving (Show)
 
