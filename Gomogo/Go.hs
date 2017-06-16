b7ddf214dd2523310aa93cb19b8b1621866ae68c
SzymonMajk <Szymek_diks@interia.pl>
2017-06-16 15:44:59 +0200

    Some refactor in Go

--- Gomogo/Go.hs
+++ Gomogo/Go.hs
@@ -2,27 +2,35 @@ import qualified Data.Matrix as M
 import qualified Data.List as L
 import qualified Data.Tree as T
 
-data Pawn = White | Black | Blank | OutOfBoard deriving (Eq)
-
-otherPawn White = Black
-otherPawn Black = White
-
-data GameBoard = GameBoard {board :: (M.Matrix Pawn)}
-
-----------------------------------   Shows   ----------------------------------
+data Pawn = White | Black | Blank deriving (Eq)
 
 instance Show Pawn where
   show (White) = show 'o'
   show (Black) = show 'x'
-  show (Blank) = show " "
+  show (Blank) = show ' '
+
+enemyPawn White = Black
+enemyPawn Black = White
+
+data GameBoard = GameBoard {board :: (M.Matrix Pawn)}
+
+--instance Show GameBoard where
+--  show (GameBoard b) = unlines ( map ((take 1 z)++) (lines (show b))) ++ --"    " ++ L.intercalate "   " [[c] | c <- z]
 
 instance Show GameBoard where
-  show (GameBoard b) = unlines ( map ((take 1 z)++) (lines (show b))) ++ "    " ++ L.intercalate "   " [[c] | c <- z]
+  show (GameBoard b) = unlines ( con (wrap size z) (lines (show b))) ++ "    " ++ L.intercalate "   " [[c] | c <- z]
 
-addLettersColumn
+con :: [String] -> [String] -> [String]
+con [] _ = []
+con _ [] = []
+con (x:xs) (y:ys) = (x ++ y):(con xs ys)
+
+wrap :: Int -> String -> [String]
+wrap 0 _ = [[]]
+wrap depth (x:xs) = [x]:(wrap (depth-1) xs)
 
 z = take size ['A'..]
-size = 2
+size = 19
 
 ----------------------------- GameBoard functions -----------------------------
 
@@ -30,39 +38,55 @@ initializeBoard :: GameBoard
 initializeBoard = GameBoard $ M.matrix size size (\(i,j) -> Blank)
 
 putPawn :: Pawn -> (Int, Int) -> GameBoard -> GameBoard
-putPawn col coords b = GameBoard $ M.setElem col coords (board b)
+putPawn p coords b = GameBoard $ M.setElem p coords (board b)
 
--- np. putPawn White (3,5) $ putPawn Black (3,6) $ putPawn White (2,5) initializeBoard
+getPawn :: GameBoard -> (Int, Int) -> Pawn
+getPawn b coords = M.getElem (fst coords) (snd coords) (board b)
+
+--test putPawn White (3,5) $ putPawn Black (3,6) $ putPawn White (2,5) initializeBoard
+
 
 allFreePositions :: GameBoard -> [(Int, Int)]
 allFreePositions (GameBoard b) = [(x,y)| x <-[1..size], y <- [1..size], (M.getElem x y b) == Blank]
 
-findPlayerPositions :: Eq a => M.Matrix a -> a -> [(Int, Int)]
-findPlayerPositions b player = [(x,y)| x <-[1..size], y <- [1..size], (M.getElem x y b) == player]
+allBusyPositions :: GameBoard -> [(Int, Int)]
+allBusyPositions (GameBoard b) = [(x,y)| x <-[1..size], y <- [1..size], (M.getElem x y b) /= Blank]
 
---findNearPositions [] = []
---findNearPositions (c:cs) = (findNearPosition c) ++ (findNearPositions cs)
---findNearPosition coords = [(x,y)| x <-[1..size], y <- [1..size], abs ((fst coords) - x) <= 1 && abs ((snd coords) - y) <= 1 && (fst coords /= x || snd coords /= y)]
+allPlayerPositions :: GameBoard -> Pawn -> [(Int, Int)]
+allPlayerPositions (GameBoard b) player = [(x,y)| x <-[1..size], y <- [1..size], (M.getElem x y b) == player]
 
+findPositionsNearPawn :: GameBoard -> [(Int, Int)] -> [(Int, Int)]
+findPositionsNearPawn b [] = []
+findPositionsNearPawn b (c:cs) = (searchBlankNear b c) ++ (findPositionsNearPawn b cs) where
+    searchBlankNear b coords = [(x,y)| x <-[1..size], y <- [1..size], (near x y coords) && (getPawn b (x,y)) == Blank]
+    near x y coords = abs ((fst coords) - x) <= 1 && abs ((snd coords) - y) <= 1
+
+--test findPositionsNearPawn (putPawn White (3,5) ( putPawn Black (3,6) ( putPawn White (2,5) initializeBoard))) $ allBusyPositions $ putPawn White (3,5) $ putPawn Black (3,6) $ putPawn White (2,5) initializeBoard
+
+{-
 findNearBlankPositions :: M.Matrix Pawn -> [(Int, Int)] -> [(Int, Int)]
 findNearBlankPositions _ [] = []
 findNearBlankPositions b (c:cs) = (findNearBlankPosition b c) ++ (findNearBlankPositions b cs)
 
 findNearBlankPosition :: M.Matrix Pawn -> (Int, Int) -> [(Int, Int)]
-findNearBlankPosition b coords = [(x,y)| x <-[1..size], y <- [1..size], abs ((fst coords) - x) <= 1 && abs ((snd coords) - y) <= 1 && (fst coords /= x || snd coords /= y) && (M.getElem x y b) == Blank]
+findNearBlankPosition b coords = [(x,y)| x <-[1..size], y <- [1..size], abs ((fst coords) - x) <= 1 && abs ((snd coords) - y) <= 1 && (fst coords /= x || snd coords /= y) ]
+-}
 
 ----------------------------  GameBoard rating  -------------------------------
 
---boardRate (GameBoard b) player = sum (ratePositions player (takeBlankPositions b (findNearPositions (findPlayerPositions b player))))
-boardRate :: Num a => GameBoard -> Pawn -> a
-boardRate (GameBoard b) player = sum (ratePositions player (findNearBlankPositions b (findPlayerPositions b player)))
+boardRate :: GameBoard -> Int
+boardRate b = (playerRate b White) - (playerRate b Black)
 
-ratePositions :: Num a => t -> [t1] -> [a]
-ratePositions player coords = map (ratePoint player) coords
+playerRate :: GameBoard -> Pawn -> Int
+playerRate b player = sum (ratePositions b player (allPlayerPositions b player))
 
-ratePoint :: Num a => t -> t1 -> a
-ratePoint player coords = 1
+ratePositions :: GameBoard -> Pawn -> [(Int, Int)] -> [Int]
+ratePositions b player coords = map (ratePoint b player) coords
+
+ratePoint :: GameBoard -> Pawn -> (Int, Int) -> Int
+ratePoint b player coords = 1
 
 ----------------------------- GameTree and MinMax -----------------------------
 
-generateGameTree gameBoard pawn = T.Node gameBoard [generateGameTree (putPawn pawn x gameBoard) (otherPawn pawn) | x <- allFreePositions gameBoard]
+--In this implementation computer cannot start if computer starts it needs to put first Pawn with different algorithm
+generateGameTree gameBoard pawn = T.Node gameBoard [generateGameTree (putPawn pawn x gameBoard) (enemyPawn pawn) | x <- findPositionsNearPawn gameBoard (allBusyPositions gameBoard)]
